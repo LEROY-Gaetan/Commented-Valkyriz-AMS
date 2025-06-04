5c71ddfa67afea6e451e6c35d36a186afff12bdd
LEROY Gaétan <gaetan.leroy@etu.ec-lyon.fr>
2025-06-04 17:31:50 +0200

    Refactor Read_data.py to improve data processing and add detailed comments

--- Read_data.py
+++ Read_data.py
@@ -1,43 +1,18 @@
-from LTC6811 import TOTAL_IC
+"""
+Lit le fichier de log binaire et affiche les données traitées de manière lisible.
+"""
+
+from LTC6811 import TOTAL_IC, NB_CELLS
 from Monitoring import MAX_MUX_PIN
 from read_temp import temp
 from ADC import convert_current
-
 import datetime
-
-filepath = "/usr/share/AMS/data/data.bin"
-
-file = open(filepath, "rb")
-
-chunksize = 8 + 2 + (12 + MAX_MUX_PIN) * 2 * TOTAL_IC
-
-data_raw = []
-
-while True:
-    chunk = file.read(chunksize)
-    data_raw.append(chunk)
-    if not chunk:
-        break
-
-data = []
-
-for raw in data_raw:
-    mes = []
-    mes.append(datetime.datetime.fromtimestamp(int.from_bytes(raw[0:8]) / 1e8))
-    mes.append(round(convert_current(int.from_bytes(raw[8:10])), 2))
-    for current_bms in range(TOTAL_IC):
-        for cell in range(12):
-            indic = (current_bms * (12 + MAX_MUX_PIN) + cell + 1) * 2 + 8
-            mes.append(round(int.from_bytes(raw[indic : indic + 2]) * 0.0001, 4))
-        for temp_n in range(MAX_MUX_PIN):
-            indic = (current_bms * (12 + MAX_MUX_PIN) + 12 + temp_n + 1) * 2 + 8
-            mes.append(
-                round(float(temp(int.from_bytes(raw[indic : indic + 2]) * 0.0001)), 4)
-            )
-    data.append(mes)
 
 
 def print_data(n: int):
+    """
+    Affiche les données d'une ligne de données traitées au preaalable.
+    """
     line = data[n]
     print("Date :", line[0])
     print("Courant (A) :", line[1])
@@ -54,5 +29,37 @@ def print_data(n: int):
     print()
 
 
-for i in range(len(data) - 1):
-    print_data(i)
+if __name__ == "__main__":
+    filepath = "/usr/share/AMS/data/data.bin"       # Chemin vers le fichier de log
+
+    file = open(filepath, "rb")     # Ouverture du fichier en mode binaire (lecture)
+
+    chunksize = 8 + 2 + (12 + MAX_MUX_PIN) * 2 * TOTAL_IC   # Taille d'un bloc de données
+
+    data_raw = []
+
+    while True:
+        chunk = file.read(chunksize)    # Lecture d'un bloc de données
+        data_raw.append(chunk)          # Ajout du bloc de données à la liste
+        if not chunk:                   # Si le bloc de données est vide, on a atteint la fin du fichier
+            break
+
+    data = []
+
+    for raw in data_raw:
+        mes = []
+        mes.append(datetime.datetime.fromtimestamp(int.from_bytes(raw[0:8]) / 1e8))     # Conversion du timestamp en date
+        mes.append(round(convert_current(int.from_bytes(raw[8:10])), 2))                # Conversion du courant en Ampères
+        for current_bms in range(TOTAL_IC):   # Pour chaque BMS
+            for cell in range(NB_CELLS):      # Pour chaque cellule du BMS k
+                indic = (current_bms * (12 + MAX_MUX_PIN) + cell + 1) * 2 + 8           # Calcul de l'indice pour accéder à la cellule
+                mes.append(round(int.from_bytes(raw[indic : indic + 2]) * 0.0001, 4))   # Conversion de la valeur de la cellule en Volts
+            for temp_n in range(MAX_MUX_PIN):   # Pour chaque capteur de température du BMS k
+                indic = (current_bms * (12 + MAX_MUX_PIN) + 12 + temp_n + 1) * 2 + 8    # Calcul de l'indice pour accéder à la température
+                mes.append(
+                    round(float(temp(int.from_bytes(raw[indic : indic + 2]) * 0.0001)), 4)    # Conversion de la valeur de la température en °C
+                )
+        data.append(mes)    # [timestamp, courant, Tensions, Températures] pour chaque BMS
+
+    for i in range(len(data) - 1):  # Affiche les données de chaque ligne
+        print_data(i)
